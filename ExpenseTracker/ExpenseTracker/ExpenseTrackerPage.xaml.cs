7caea7ab4019f1eb77005f78701208bc28c3a3e3
Vikram Gore <vikramgore2007@gmail.com>
2017-03-29 19:49:40 +0530

    Added base project and added git ignore

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ExpenseTracker
+{
+	public partial class ExpenseTrackerPage : ContentPage
+	{
+		public ExpenseTrackerPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
